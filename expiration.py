15917bb229bd02f1f84f73e060db9358a6fcaa95
nohdol97 <nohdol97@gmail.com>
2024-02-21 21:48:11 +0900

    init

--- expiration.py
+++ expiration.py
@@ -3,12 +3,9 @@ from dateutil.relativedelta import relativedelta
 
 
 now = date.today()
-three_days_later = now + relativedelta(days=3)
 one_month_later = now + relativedelta(months=1)
 
-# print(f"3일 후\ndate({three_days_later.year}, {three_days_later.month}, {three_days_later.day})")
 # print(f"한달 후\ndate({one_month_later.year}, {one_month_later.month}, {one_month_later.day})")
 
 # 만료 일정 (위의 출력 후 결정)
-# expiration_date = date(2024, 2, 17)
-expiration_date = ""
+# expiration_date = date(2024, 12, 17)
