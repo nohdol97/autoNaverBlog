5b25358100e1160113c1cb11b3a638dfcfb1bb3e
nohdol97 <nohdol97@gmail.com>
2024-05-17 14:00:14 +0900

    edit leaveComment

--- comment.py
+++ comment.py
@@ -28,7 +28,7 @@ def getComment():
     random_comment = random.choice(comment_list1) + "\n" + random.choice(comment_list2)
     return random_comment
 
-def leaveComment(driver, is_secret):
+def leaveComment(driver, is_secret, is_leaveComment):
     try:
         name = ""
         try:
@@ -45,49 +45,50 @@ def leaveComment(driver, is_secret):
         else:
             name_dict[name] = 1
             time.sleep(1)
-            # 댓글 달기로 이동
-            try:
-                link_xpath = '//*[@id="ct"]/div[4]/div[4]/div/div[2]/a[1]/span'
-                link_element = driver.find_element(By.XPATH, link_xpath)
-                link_element.click()
-            except:
-                link_xpath = "//*[@id='ct']/div[4]/div[3]/div/div[2]/a[1]"
-                link_element = driver.find_element(By.XPATH, link_xpath)
-                link_element.click()
-            time.sleep(2)
-            # 댓글 공간
-            comment_label_xpath = '//*[@id="naverComment__write_textarea"]'
-            comment_label_element = driver.find_element(By.XPATH, comment_label_xpath)
-            # 댓글 작성
-            randomValue = random.random()
-            if randomValue < 0.2: # 20퍼 확률
-                pyperclip.copy(str(name) + "님 :)\n" + getComment())
-                comment_label_element.send_keys(Keys.CONTROL,'v')
-            elif randomValue < 0.4: # 20퍼 확률
-                pyperclip.copy(str(name) + "님~\n" + getComment())
-                comment_label_element.send_keys(Keys.CONTROL,'v')
-            elif randomValue < 0.6: # 20퍼 확률
-                pyperclip.copy("안녕하세요! " + str(name) + "님~\n" + getComment())
-                comment_label_element.send_keys(Keys.CONTROL,'v')
-            elif randomValue < 0.8: # 20퍼 확률
-                pyperclip.copy(str(name) + "님 :) 오늘도 방문했어요 ㅎㅎ\n" + getComment())
-                comment_label_element.send_keys(Keys.CONTROL,'v')
-            else: # 30퍼 확률
-                pyperclip.copy(getComment())
-                comment_label_element.send_keys(Keys.CONTROL,'v')
-            time.sleep(2)
-            # 비밀글로 할지
-            if(is_secret and util.random_choice_with_probability(0.3)):
-                secret_xpath = '//*[@id="naverComment__write_textarea_secret_check"]'
-                secret_element = driver.find_element(By.XPATH, secret_xpath)
-                secret_element.click()
+            if (is_leaveComment):
+                # 댓글 달기로 이동
+                try:
+                    link_xpath = '//*[@id="ct"]/div[4]/div[4]/div/div[2]/a[1]/span'
+                    link_element = driver.find_element(By.XPATH, link_xpath)
+                    link_element.click()
+                except:
+                    link_xpath = "//*[@id='ct']/div[4]/div[3]/div/div[2]/a[1]"
+                    link_element = driver.find_element(By.XPATH, link_xpath)
+                    link_element.click()
                 time.sleep(2)
-            # 댓글 제출
-            subit_xpath = '//*[@id="naverComment"]/div/div[7]/div[1]/form/fieldset/div/div/div[6]/button'
-            submit_element = driver.find_element(By.XPATH, subit_xpath)
-            submit_element.click()
-            time.sleep(2)
-            action.closeBlog(driver) # 종료
+                # 댓글 공간
+                comment_label_xpath = '//*[@id="naverComment__write_textarea"]'
+                comment_label_element = driver.find_element(By.XPATH, comment_label_xpath)
+                # 댓글 작성
+                randomValue = random.random()
+                if randomValue < 0.2: # 20퍼 확률
+                    pyperclip.copy(str(name) + "님 :)\n" + getComment())
+                    comment_label_element.send_keys(Keys.CONTROL,'v')
+                elif randomValue < 0.4: # 20퍼 확률
+                    pyperclip.copy(str(name) + "님~\n" + getComment())
+                    comment_label_element.send_keys(Keys.CONTROL,'v')
+                elif randomValue < 0.6: # 20퍼 확률
+                    pyperclip.copy("안녕하세요! " + str(name) + "님~\n" + getComment())
+                    comment_label_element.send_keys(Keys.CONTROL,'v')
+                elif randomValue < 0.8: # 20퍼 확률
+                    pyperclip.copy(str(name) + "님 :) 오늘도 방문했어요 ㅎㅎ\n" + getComment())
+                    comment_label_element.send_keys(Keys.CONTROL,'v')
+                else: # 30퍼 확률
+                    pyperclip.copy(getComment())
+                    comment_label_element.send_keys(Keys.CONTROL,'v')
+                time.sleep(2)
+                # 비밀글로 할지
+                if(is_secret and util.random_choice_with_probability(0.3)):
+                    secret_xpath = '//*[@id="naverComment__write_textarea_secret_check"]'
+                    secret_element = driver.find_element(By.XPATH, secret_xpath)
+                    secret_element.click()
+                    time.sleep(2)
+                # 댓글 제출
+                subit_xpath = '//*[@id="naverComment"]/div/div[7]/div[1]/form/fieldset/div/div/div[6]/button'
+                submit_element = driver.find_element(By.XPATH, subit_xpath)
+                submit_element.click()
+                time.sleep(2)
+                action.closeBlog(driver) # 종료
     except:
         action.closeBlog(driver) # 종료
         pass
